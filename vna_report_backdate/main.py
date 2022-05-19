6fa33ae3d8f6847e443bd42ed697d7a81f2a8d49
Weed <nam.mk@urbox.vn>
2022-05-19 17:37:17 +0700

    Test

--- vna_report_backdate/main.py
+++ vna_report_backdate/main.py
@@ -3,32 +3,34 @@ from Email import Email
 
 import pandas
 import psycopg2
+import os
 
 redshift_connection = psycopg2.connect(**REDSHIFT_CONFIG)
 
 
 def main(report_date):
     try:
-        query = f"""
-            CALL ub_ods.pcd_daily_export_revenue_data_vna_cancel('{report_date}', '{report_date}', 'tmp_table');
-            SELECT * FROM tmp_table;
-        """
-        # report_date = report_date.strftime('%d%m%Y')
-        order_list_cancel = pandas.read_sql(query, redshift_connection)
-        redshift_connection.commit()
-        redshift_connection.close()
-        order_list_cancel.to_csv("nammk_test.csv", index=False)
-        Email.send_mail_with_attachment("nam.mk@urbox.vn", "test subject", "test_content", ['nammk_test.csv'])
-        # with open("test.csv", "w") as file:
-        #     file.write(order_list_cancel.to_csv(index=False))
-        # file.close()
-
-        # s = sftp.Connection(host=host, username='urbox', password='Jul#020721Evoucher')
-        # remote_path = "/urbox_data/"
+        print(os.getcwd())
+        # query = f"""
+        #     CALL ub_ods.pcd_daily_export_revenue_data_vna_cancel('{report_date}', '{report_date}', 'tmp_table');
+        #     SELECT * FROM tmp_table;
+        # """
+        # # report_date = report_date.strftime('%d%m%Y')
+        # order_list_cancel = pandas.read_sql(query, redshift_connection)
+        # redshift_connection.commit()
+        # redshift_connection.close()
+        # order_list_cancel.to_csv("nammk_test.csv", index=False)
+        # Email.send_mail_with_attachment("nam.mk@urbox.vn", "test subject", "test_content", ['nammk_test.csv'])
+        # # with open("test.csv", "w") as file:
+        # #     file.write(order_list_cancel.to_csv(index=False))
+        # # file.close()
         #
-        # file = f"VNA_Evoucher_Orderlist_Cancel/VNA_Evoucher_Orderlist_Cancel_{report_date}{ext}
-        # with s.open(remote_path + file, "w") as f:
-        #     f.write(order_list_cancel.to_csv(index=False))
-        # s.close()
+        # # s = sftp.Connection(host=host, username='urbox', password='Jul#020721Evoucher')
+        # # remote_path = "/urbox_data/"
+        # #
+        # # file = f"VNA_Evoucher_Orderlist_Cancel/VNA_Evoucher_Orderlist_Cancel_{report_date}{ext}
+        # # with s.open(remote_path + file, "w") as f:
+        # #     f.write(order_list_cancel.to_csv(index=False))
+        # # s.close()
     except Exception as e:
         print(e)
